0d58775d6175658331183fd437244f83cf42e0e0
RoheadOfficial <studios.hmlhaus@gmail.com>
2026-04-15 13:27:12 +0800

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express, { type Express } from "express";
+import cors from "cors";
+import router from "./routes";
 
-const app = express();
+const app: Express = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api", (req, res) => {
-  res.send("API working");
-});
+app.use("/api", router);
 
 export default app;
