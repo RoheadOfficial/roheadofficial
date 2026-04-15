f7697902d3d94d9f531334c7034875c212902663
RoheadOfficial <studios.hmlhaus@gmail.com>
2026-04-15 13:24:57 +0800

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -3,7 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Server is running");
+});
+
+app.get("/api", (req, res) => {
+  res.send("API working");
 });
 
 export default app;
